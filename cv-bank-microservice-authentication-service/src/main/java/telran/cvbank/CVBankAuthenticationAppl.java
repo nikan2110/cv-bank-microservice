a229722d23b04d79b555d80b3e4e6f419c22f1d2
nikan2110 <nikan2110@gmail.com>
2022-07-04 16:45:24 +0300

    Fixed bugs

--- cv-bank-microservice-authentication-service/src/main/java/telran/cvbank/CVBankAuthenticationAppl.java
+++ cv-bank-microservice-authentication-service/src/main/java/telran/cvbank/CVBankAuthenticationAppl.java
@@ -2,12 +2,12 @@ package telran.cvbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class CVBankAuthenticationAppl {
 
 	public static void main(String[] args) {
