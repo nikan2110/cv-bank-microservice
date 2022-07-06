a6350adb590e5450270400ff4632bc522e914a0d
nikan2110 <nikan2110@gmail.com>
2022-07-06 10:48:43 +0300

    Fixed first 3 junits test

--- cv-bank-microservice-employee-service/src/test/java/telran/cvbank/CvbankApplicationEmployeeTests.java
+++ cv-bank-microservice-employee-service/src/test/java/telran/cvbank/CvbankApplicationEmployeeTests.java
@@ -1,13 +1,17 @@
 package telran.cvbank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.HashSet;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.modelmapper.ModelMapper;
@@ -18,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,7 +38,7 @@ import telran.cvbank.exceptions.EmployeeAlreadyExistException;
 import telran.cvbank.model.Employee;
 import telran.cvbank.service.EmployeeAccountServiceImpl;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.cloud.config.enabled=false" })
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @EnableConfigurationProperties
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
@@ -59,77 +64,80 @@ class CvbankApplicationEmployeeTests {
 		this.restTemplate = restTemplate;
 	}
 
-	Employee employee = Employee.builder().email("green@goblin.com").password("123456").firstName("Norman")
+	Employee employee = Employee.builder()
+			.email("green@goblin.com")
+			.password("123456")
+			.firstName("Norman")
 			.lastName("Osborn").cv_id(new HashSet<>()).build();
 
-	UpdateEmployeeDto updateEmployeeDto = UpdateEmployeeDto.builder().firstName("Harry").lastName("Osborn").build();
+	UpdateEmployeeDto updateEmployeeDto = UpdateEmployeeDto.builder()
+			.firstName("Harry")
+			.lastName("Osborn")
+			.build();
 
 	@Test
+	@Order(1)
 	public void addEmployee() throws Exception {
-		System.out.println("1 test");
         RegisterEmployeeDto newEmployeeDto = modelMapper.map(employee, RegisterEmployeeDto.class);
-		System.out.println("2 test");
         assertEquals(employee.getEmail(), employeeService.registerEmployee(newEmployeeDto).getEmail());
-        System.out.println("3 test");
-//        String json = convertToJson(newEmployeeDto);
-//        mockMvc.perform(post(BASE_URL_EMPLOYEE + "/signup")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isConflict())
-//                .andExpect(result -> assertTrue(result.getResponse().getContentAsString().contains("Employee already exist")))
-//                .andExpect(result -> assertTrue(result.getResolvedException() instanceof EmployeeAlreadyExistException));
+        String json = convertToJson(newEmployeeDto);
+        mockMvc.perform(post(BASE_URL_EMPLOYEE + "/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isConflict())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof EmployeeAlreadyExistException));
 	}
 
-//    @Test
-//    @Order(2)
-//    @WithMockUser(username = "test", password = "test", roles = "EMPLOYER")
-//    public void getEmployeeIfEmployer() throws Exception {
-//        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
-//                .andExpect(status().isForbidden());
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @WithMockUser(username = "green@goblin.com", password = "123456", roles = "EMPLOYEE")
-//    public void getEmployeeIfEmployeeOwner() throws Exception {
-//        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
-//                .andExpect(status().isOk());
-//        assertNotNull(employeeService.getEmployee("green@goblin.com"));
-//        assertEquals("Norman", employeeService.getEmployee("green@goblin.com").getFirstName());
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @WithMockUser(username = "test", password = "test", roles = "EMPLOYEE")
-//    public void getEmployeeIfEmployeeNotOwner() throws Exception {
-//        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
-//                .andExpect(status().isForbidden());
-//    }
-//
-//    @Test
-//    @Order(10)
-//    @WithMockUser(username = "green@goblin.com", password = "123456", roles = "EMPLOYEE")
-//    public void updateEmployeeIfEmployeeOwner() throws Exception {
-//        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-//        objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
-//        String json = objectWriter.writeValueAsString(updateEmployeeDto);
-//        mockMvc.perform(put(BASE_URL_EMPLOYEE + "/green@goblin.com")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isOk());
-//        assertEquals("Harry", employeeService.getEmployee("green@goblin.com").getFirstName());
-//    }
-//
-//    @Test
-//    @Order(10)
-//    @WithMockUser(username = "test", password = "test", roles = "EMPLOYEE")
-//    public void updateEmployeeIfEmployeeNotOwner() throws Exception {
-//        String json = convertToJson(updateEmployeeDto);
-//        mockMvc.perform(put(BASE_URL_EMPLOYEE + "/green@goblin.com")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isForbidden());
-//    }
+    @Test
+    @Order(2)
+    @WithMockUser(username = "test", password = "test", roles = "EMPLOYER")
+    public void getEmployeeIfEmployer() throws Exception {
+        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(2)
+    @WithMockUser(username = "green@goblin.com", password = "123456", roles = "EMPLOYEE")
+    public void getEmployeeIfEmployeeOwner() throws Exception {
+        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
+                .andExpect(status().isOk());
+        assertNotNull(employeeService.getEmployee("green@goblin.com"));
+        assertEquals("Norman", employeeService.getEmployee("green@goblin.com").getFirstName());
+    }
+
+    @Test
+    @Order(2)
+    @WithMockUser(username = "test", password = "test", roles = "EMPLOYEE")
+    public void getEmployeeIfEmployeeNotOwner() throws Exception {
+        mockMvc.perform(get(BASE_URL_EMPLOYEE + "/green@goblin.com"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(10)
+    @WithMockUser(username = "green@goblin.com", password = "123456", roles = "EMPLOYEE")
+    public void updateEmployeeIfEmployeeOwner() throws Exception {
+        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        String json = objectWriter.writeValueAsString(updateEmployeeDto);
+        mockMvc.perform(put(BASE_URL_EMPLOYEE + "/green@goblin.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+        assertEquals("Harry", employeeService.getEmployee("green@goblin.com").getFirstName());
+    }
+
+    @Test
+    @Order(10)
+    @WithMockUser(username = "test", password = "test", roles = "EMPLOYEE")
+    public void updateEmployeeIfEmployeeNotOwner() throws Exception {
+        String json = convertToJson(updateEmployeeDto);
+        mockMvc.perform(put(BASE_URL_EMPLOYEE + "/green@goblin.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isForbidden());
+    }
 //
 //    @Test
 //    @Order(10)
