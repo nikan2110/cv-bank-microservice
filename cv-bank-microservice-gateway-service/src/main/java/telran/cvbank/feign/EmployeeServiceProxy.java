a753c5e87eff5e81cc8b4f8ba9ab059056544a40
nikan2110 <nikan2110@gmail.com>
2022-07-11 12:32:19 +0300

    Added reactive feign to gateway service

---
+++ cv-bank-microservice-gateway-service/src/main/java/telran/cvbank/feign/EmployeeServiceProxy.java
@@ -0,0 +1,19 @@
+package telran.cvbank.feign;
+
+
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import reactivefeign.spring.config.ReactiveFeignClient;
+import reactor.core.publisher.Mono;
+import telran.cvbank.model.Employee;
+
+@LoadBalancerClient(name = "cv-bank-microservice-employee-service")
+@ReactiveFeignClient(name="cv-bank-microservice-employee-service")
+public interface EmployeeServiceProxy {
+	
+	@GetMapping("/cvbank/employee/feign/{id}")
+	public Mono<Employee> getEmployeeById(@PathVariable String id);
+
+}
---
+++ cv-bank-microservice-gateway-service/src/main/java/telran/cvbank/model/Employee.java
@@ -0,0 +1,27 @@
+package telran.cvbank.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Employee {
+
+    String email;
+    String password;
+    String firstName;
+    String lastName;
+    Set<String> cv_id = new HashSet<>();
+    Set<String> roles = new HashSet<>();
+}
